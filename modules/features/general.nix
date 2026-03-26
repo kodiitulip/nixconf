69fa7e99838594443521f7e2350b306d7de0c55f
kodiitulip <kodii.tulip@proton.me>
2026-03-25 21:13:36 -0300

    general module

---
+++ modules/features/general.nix
@@ -0,0 +1,47 @@
+{ self, ... }:
+{
+  flake.nixosModules.general =
+    {
+      pkgs,
+      config,
+      ...
+    }:
+    {
+      imports = [
+        # self.nixosModules.extra_hjem
+        # self.nixosModules.gtk
+        self.nixosModules.nix
+      ];
+
+      users.users.${config.preferences.user.name} = {
+        isNormalUser = true;
+        description = "${config.preferences.user.name}'s account";
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+        ];
+        shell = self.packages.${pkgs.system}.environment;
+
+        initialPassword = "${config.preferences.user.name}";
+      };
+
+      # persistance.data.directories = [
+      #   "nixconf"
+      #
+      #   "Videos"
+      #   "Documents"
+      #   "Projects"
+      #
+      #   ".ssh"
+      # ];
+      #
+      # # todo: remove
+      # persistance.cache.directories = [
+      #   ".local/share/zoxide"
+      #   ".local/share/direnv"
+      #   ".local/share/nvim"
+      #   ".local/share/fish"
+      #   ".config/nvim"
+      # ];
+    };
+}
