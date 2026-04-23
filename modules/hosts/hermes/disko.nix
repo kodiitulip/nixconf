065619da55efd7a15ed051e68196c4cab9535c69
kodiitulip <kodii.tulip@proton.me>
2026-04-23 08:38:03 -0300

    disko setup for hermes

--- modules/hosts/hermes/disko.nix
+++ modules/hosts/hermes/disko.nix
@@ -1,5 +1,85 @@
 {
-  flake.diskoConfigurations.hermes = _: {
-    # TODO: Setup Hermes' disko config
-  };
+  flake.diskoConfigurations.hermes =
+    { lib, ... }:
+    {
+      disko.devices = {
+        disk.main = {
+          device = lib.mkDefault "/dev/sda";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                name = "boot";
+                size = "1M";
+                type = "EF02";
+              };
+              esp = {
+                name = "ESP";
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              };
+              root = {
+                name = "root";
+                size = "100%";
+                content = {
+                  type = "lvm_pv";
+                  vg = "btrfs_vg";
+                };
+              };
+            };
+          };
+        };
+        nodev = {
+          "/" = {
+            fsType = "tmpfs";
+            mountOptions = [
+              "size=25%"
+              "mode=755"
+            ];
+          };
+        };
+        lvm_vg = {
+          btrfs_vg = {
+            type = "lvm_vg";
+            lvs = {
+              root = {
+                size = "100%FREE";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+
+                  subvolumes = {
+                    "/root" = {
+                      # mountpoint = "/";
+                    };
+
+                    "/persist" = {
+                      mountOptions = [
+                        "subvol=persist"
+                        "noatime"
+                      ];
+                      mountpoint = "/persist";
+                    };
+
+                    "/nix" = {
+                      mountOptions = [
+                        "subvol=nix"
+                        "noatime"
+                      ];
+                      mountpoint = "/nix";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
 }
