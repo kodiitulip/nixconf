ce7f854283296fe5ae510ff9ad79f2d958401754
kodiitulip <kodii.tulip@proton.me>
2026-05-02 00:10:58 -0300

    ryujinx

--- modules/features/gaming.nix
+++ modules/features/gaming.nix
@@ -1,12 +1,18 @@
-{ inputs, ... }:
+{
+  self,
+  inputs,
+  ...
+}:
 {
   flake.nixosModules.gaming =
     {
       pkgs,
       lib,
-      self',
       ...
     }:
+    let
+      selfpkgs = self.packages.${pkgs.stdenv.hostPlatform.system};
+    in
     {
       imports = [ inputs.vintagestory-nix.homeModules.default ];
 
@@ -60,7 +66,7 @@
             temurin-bin-17
           ];
         })
-        self'.packages.hyprism
+        selfpkgs.hyprism
         easyeffects
         rose-pine-cursor
         protonplus
@@ -68,12 +74,13 @@
         mindustry
         vintagestoryPackages.rustique
         vintagestoryPackages.vs-launcher
+        selfpkgs.ryujinx
 
         dxvk
         gamescope
         mangohud
         r2modman
-        self'.packages.mrpack-install
+        selfpkgs.mrpack-install
 
         # NOTE: maybe when I buy lossless scaling?
         # lsfg-vk
@@ -140,6 +147,48 @@
             [ "-skip=^${builtins.concatStringsSep "$|^" skippedTests}$" ];
           postInstall = "";
         });
+
+        ryujinx =
+          let
+            pname = "ryujinx";
+            version = "1.3.280";
+
+            src = pkgs.fetchurl {
+              url = "https://git.ryujinx.app/Ryubing/Canary/releases/download/${version}/ryujinx-canary-${version}-x64.AppImage";
+              hash = "sha256-WMJb9fsPB5hUI2SNapn6jN4+JZFhP096JVRp+iTggEg=";
+            };
+
+            contents = pkgs.appimageTools.extractType2 {
+              inherit pname version src;
+            };
+          in
+          pkgs.appimageTools.wrapType2 {
+            inherit pname version src;
+            extraPkgs =
+              pkgs: with pkgs; [
+                icu
+                lttng-ust
+              ];
+
+            extraInstallCommands = ''
+              mkdir -pv $out/share/applications $out/share/icons/hicolor/512x512/apps
+
+              install -m 444 ${contents}/Ryujinx.desktop \
+                $out/share/applications/${pname}.desktop
+              install -m 444 ${contents}/Ryujinx.svg \
+                $out/share/icons/hicolor/512x512/apps/${pname}.svg
+
+              substituteInPlace $out/share/applications/${pname}.desktop \
+                --replace-fail 'Exec=Ryujinx.sh' 'Exec=${pname}' \
+                --replace-fail 'Icon=Ryujinx' 'Icon=${pname}'
+            '';
+
+            meta = {
+              description = "Ryujinx is an open-source Nintendo Switch emulator, originally created by gdkchan, written in C#.";
+              homepage = "https://git.ryujinx.app/Ryubing";
+              platforms = [ "x86_64-linux" ];
+            };
+          };
 
         hyprism =
           let
